6795af67df54b183684be00a41d7480d0d3fabf2
Nicco <niccolino707@yahoo.com>
2022-01-04 17:18:38 +0800

    Added html, js and css files

---
+++ script.js
@@ -0,0 +1,16 @@
+let myLibrary = []
+
+function Book(name,author,pages,read) {
+    this.name = name
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+
+
+function addBookToLibrary(item) {
+    myLibrary.push(item)
+}
+
+
+let bookOne = new Book('Dunno','Nicco',20,false)
